cab5a34c0a8fcb09fab6e8bce225c15e2c49b995
Saurabh2862 <saurabhp.ug21.ec@nitp.ac.in>
2025-06-08 09:50:00 +0530

    Update robots.txt with correct sitemap location

--- pages/robots.txt.js
+++ pages/robots.txt.js
@@ -2,8 +2,9 @@ export async function getServerSideProps({ res }) {
     res.setHeader("Content-Type", "text/plain");
     res.write(`User-agent: *
   Allow: /
-  Sitemap: https://bellpost-seo.vercel.app/sitemap.xml`);
+  Sitemap: https://bellpost-seo.vercel.app/sitemap_bellpost.xml`);
     res.end();
+  
     return { props: {} };
   }
   
