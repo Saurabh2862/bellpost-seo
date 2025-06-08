0956343991dfa7f748a7ba353ee53075afce8687
Saurabh-Bellpost <saurabh@bellpost.com>
2025-06-08 09:09:01 +0530

    Add sitemap_bellpost.xml for Google indexing

--- pages/sitemap_bellpost.xml.js
+++ pages/sitemap_bellpost.xml.js
@@ -3,31 +3,31 @@ import { slugify } from "../lib/slugify";
 
 const SITE_URL = "https://bellpost-seo.vercel.app";
 
-// Optional: if each bell has a `createdAt` or `updatedAt` timestamp, use that.
-const FAKE_LASTMOD = new Date().toISOString();
+// In future, replace with bell.updatedAt or bell.createdAt
+const DEFAULT_LASTMOD = new Date().toISOString();
 
 export const getServerSideProps = async ({ res }) => {
   const urls = bells.map((bell) => {
     const slug = slugify(bell.title);
     const loc = `${SITE_URL}/bells/${slug}`;
-    const lastmod = FAKE_LASTMOD;
+    const lastmod = DEFAULT_LASTMOD;
 
     return `
-      <url>
-        <loc>${loc}</loc>
-        <lastmod>${lastmod}</lastmod>
-        <changefreq>monthly</changefreq>
-        <priority>0.9</priority>
-      </url>`;
+<url>
+  <loc>${loc}</loc>
+  <lastmod>${lastmod}</lastmod>
+  <changefreq>monthly</changefreq>
+  <priority>0.9</priority>
+</url>`;
   }).join("");
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset
+<urlset 
   xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
                       http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-  ${urls}
+${urls}
 </urlset>`;
 
   res.setHeader("Content-Type", "text/xml");
@@ -37,6 +37,6 @@ export const getServerSideProps = async ({ res }) => {
   return { props: {} };
 };
 
-export default function Sitemap() {
+export default function SitemapBellpost() {
   return null;
 }
