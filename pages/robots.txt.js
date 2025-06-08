b1b3ccb48d349bfd40a0921a5589db04a5a60723
Saurabh2862 <saurabhp.ug21.ec@nitp.ac.in>
2025-06-08 10:36:54 +0530

    update

--- pages/robots.txt.js
+++ pages/robots.txt.js
@@ -1,13 +1,17 @@
+// pages/robots.txt.js
 export async function getServerSideProps({ res }) {
-    res.setHeader("Content-Type", "text/plain");
-    res.write(`User-agent: *
+    const robotsContent = `User-agent: *
   Allow: /
-  Sitemap: https://bellpost-seo.vercel.app/sitemap_bellpost.xml`);
+  Sitemap: https://bellpost-seo.vercel.app/sitemap_bellpost.xml`;
+  
+    res.setHeader("Content-Type", "text/plain");
+    res.setHeader("Cache-Control", "public, s-maxage=86400, stale-while-revalidate");
+    res.write(robotsContent);
     res.end();
+    
     return { props: {} };
   }
   
   export default function Robots() {
     return null;
   }
-  
